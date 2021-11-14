1df6745baebded8b7e4855590d5e796ad2356955
ejvargas <ejvargas@gmail.com>
2021-11-13 20:39:57 -0500

    Update DataSourceConfiguration.java

--- src/main/java/co/com/ejvt/ml/mutant/DataSourceConfiguration.java
+++ src/main/java/co/com/ejvt/ml/mutant/DataSourceConfiguration.java
@@ -16,7 +16,7 @@ public class DataSourceConfiguration {
 
 	//private String jdbcUrl = "jdbc:postgresql://localhost:5432/mutant?user=postgres&password=pro";
 	private String jdbcUrl = System.getenv().get("JDBC_DATABASE_URL") == null ? "jdbc:postgresql://localhost:5432/mutant?user=postgres&password=pro"
-			: "jdbc:"+System.getenv().get("JDBC_DATABASE_URL");
+			: System.getenv().get("JDBC_DATABASE_URL");
 	
     @Bean(name="customDataSource")
     @ConfigurationProperties("spring.datasource")
